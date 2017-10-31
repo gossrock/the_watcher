bf8005e6991b9a639955fda38fc346a2a1f4fbd8
gossrock <gossrock@gmail.com>
2017-10-31 10:20:09 +0000

    added the basics of keyboard input (wasted a lot of time becasue I forgot that my UI takes 'FPS' and not 'time between updates')

--- async_curses.py
+++ async_curses.py
@@ -13,10 +13,17 @@ class Window:
 		represents a window space on the screen that you can place text in
 	"""
 	contents = None
-	def __init__(self, parent, height, width, y_start, x_start):
+	def __init__(self, parent, height=-1, width=-1, y_start=0, x_start=0):
+		if height == -1:
+			height = parent.getmaxyx()[0]
+		if width == -1:
+			width = parent.getmaxyx()[1]
+			
 		self.parent = parent
+		self.parent.nodelay(True)
 		self.text_area = self.parent.derwin(height, width, y_start, x_start)
 		self.text_area.scrollok(True)
+		self.text_area.nodelay(True)
 		self.Contents = ''
 
 	@property
@@ -54,13 +61,20 @@ class BorderedWindow(Window):
 	'''
 		a sub class of Window that simply has a border around it.
 	'''
-	def __init__(self, parent, height, width, y_start, x_start):
+	def __init__(self, parent, height=-1, width=-1, y_start=0, x_start=0):
+		if height == -1:
+			height = parent.getmaxyx()[0]
+		if width == -1:
+			width = parent.getmaxyx()[1]
+			
 		self.parent = parent
+		self.parent.nodelay(True)
 		self.border = self.parent.derwin(height, width, y_start, x_start)
 		self.border.box()
 		self.border.noutrefresh()
 		self.text_area = self.parent.derwin(height-2, width-2, y_start+1, x_start+1)
 		self.text_area.scrollok(True)
+		self.text_area.nodelay(True)
 		self.Contents = ''
 
 
@@ -72,6 +86,7 @@ class TableLayout:
 	'''
 	def __init__(self, parent, rows, cols, window_type=Window):
 		self.parent = parent
+		self.parent.nodelay(True)
 		self.sub_windows = [ [ None for i in range(cols) ] for j in range(rows) ]
 		maxy, maxx = self.parent.getmaxyx()
 		height = math.floor(maxy/rows)
@@ -104,14 +119,15 @@ class BaseUI:
 			initalizes this UI for use in context managers
 		'''
 		self.main_window = curses.initscr()
+		self.main_window.nodelay(True)
+		self.main_window.keypad(True)
+		curses.noecho()
 		curses.start_color()
 		curses.init_pair(WHITE, curses.COLOR_WHITE, curses.COLOR_BLACK)
 		curses.init_pair(RED, curses.COLOR_RED, curses.COLOR_BLACK)
 		curses.init_pair(GREEN, curses.COLOR_GREEN, curses.COLOR_BLACK)
 
-		self.main_window.nodelay(True)
-		self.main_window.keypad(True)
-		curses.noecho()
+		
 		self.setup()
 		return self
 		
@@ -153,9 +169,27 @@ class BaseUI:
 			self.cleanup()
 			self.close = True
 			return
-		
+	
+	async def keyboard_listener(self):
+		while True:
+			key = await self.get_key()
+			if key is None:
+				continue
+			else:
+				self.key_stroke_handler(key)
 			
+	
+	async def get_key(self):
+		await asyncio.sleep(0.1)
+		ch = None
+		try:
+			ch = self.main_window.get_wch()
+		except curses.error as e:
+			return None
+		return ch
 		
+	def key_stroke_handler(self, key):
+		pass
 
 
 class TestingUI_1(BaseUI):
@@ -254,10 +288,20 @@ class TestingUI_3(BaseUI):
 			self.cleanup()
 			self.close = True
 			return
+
+class TestingUI_4(BaseUI):
+	'''
+	'''
+	def setup(self):
+		self.text_window = BorderedWindow(self.main_window)
 		
+	def key_stroke_handler(self, key):
+		self.text_window.add(str(key))
+	
 
 def run_testing_ui():
 	try:
+		'''
 		with TestingUI_1(frame_rate=1) as ui:
 			loop = asyncio.get_event_loop()
 			asyncio.ensure_future(ui.test_worker())
@@ -272,6 +316,11 @@ def run_testing_ui():
 		with TestingUI_3(frame_rate=1) as ui:
 			loop = asyncio.get_event_loop()
 			asyncio.ensure_future(ui.test_worker())
+			loop.run_until_complete(ui.screen_updater())
+		'''
+		with TestingUI_4(frame_rate=10) as ui:
+			loop = asyncio.get_event_loop()
+			asyncio.ensure_future(ui.keyboard_listener())
 			loop.run_until_complete(ui.screen_updater())
 	except KeyboardInterrupt:
 		print('ending program')
