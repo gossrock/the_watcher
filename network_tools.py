eb8e7144c9da2bd3d7f107874593b2ffd40bb456
gossrock <gossrock@gmail.com>
2017-10-31 08:51:46 +0000

    stating ping scanner implementation

--- network_tools.py
+++ network_tools.py
@@ -1,4 +1,6 @@
 import asyncio
+import ipaddress
+
 
 async def run_command(*args):
 	process = await asyncio.create_subprocess_exec( *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
@@ -8,14 +10,20 @@ async def run_command(*args):
 	return (out, error)
 
 async def ping(host):
-	return await run_command('ping', '-c', '1', host)
+	return await run_command('ping', '-c', '1', '-W', '1', host)
 
+
+async def ping_scan(network):
+	for host in ipaddress.ip_network(network):
+		out, error = await ping(str(host))
+	return (out, error)
+	
 
 def run_test():
 	
 
 	loop = asyncio.get_event_loop()
-	commands = asyncio.gather(ping('google.com'), ping('10.10.1.1'))
+	commands = asyncio.gather(ping('google.com'), ping('10.10.1.1'), ping_scan('10.10.1.0/24'))
 	# Run the commands
 	results = loop.run_until_complete(commands)
 	# Print a report
