23ce9c812300c00d95166b5170e7e349ee61e2c2
gossrock <gossrock@gmail.com>
2017-11-08 13:01:50 +0000

    Worked out a sceem to simplify asyncio a bit for a curses ui I think

--- experiments/async_curses_panels.py
+++ experiments/async_curses_panels.py
@@ -1,5 +1,79 @@
 import asyncio
 import curses
+import time
+
+DEFAULT_COLOR = 1
+
+
+#### ASYNC UTILS ####
+def keyboard_interuptable_loop_coroutine(f):
+	async def func_wrapper(*args, **kwargs):
+		self = args[0]
+		name = args[1]
+		print(f'{__name__}')
+		while self.close == False:
+			try:
+				await asyncio.sleep(0)
+				await f(*args, **kwargs)
+			except KeyboardInterrupt as kbi:
+				print(f'KeyboardInterup in keyboard_inteupable_loop_coroutine func_wrapper: {name}')
+				self.close = True
+				
+		print(f'ending keyboard_inteupable_loop_coroutine func_wrapper: {name}: self.close: {self.close}')
+	return func_wrapper
+
+async def keep_running_till_all_others_complete(loop):
+	tasks = asyncio.Task.all_tasks(loop)
+	print('starting up loop_closer')
+	not_done = True
+	while not_done:
+		not_done = False
+		for task in tasks:
+			if task.done() == False and task is not asyncio.Task.current_task(loop):
+				not_done = True
+				break
+		await asyncio.sleep(0.1)
+	
+	print(f'closing down loop')
+
+class TaskLoopBase:
+	def __init__(self):
+		self.close = False
+		self.tasks = []
+		
+	def __enter__(self):
+		return self
+		
+	def __exit__(self, *args):
+		self.cleanup()
+		
+	def cleanup(self):
+		pass
+		
+	
+	
+	def add_task(self, task):
+		self.tasks.append(task)
+	
+	def run_until_complete(self):
+		loop = asyncio.get_event_loop()
+		try:
+			for task in self.tasks:
+				asyncio.ensure_future(task)
+			# This is for when a KeyboardInterupt happens inside a @keyboard_interuptable_loop_coroutine
+			# this will more likely happen when there is more work to do in side the worker coroutines (some slight blocking)
+			loop.run_until_complete(keep_running_till_all_others_complete(loop)) 
+		except KeyboardInterrupt as kbi:
+			print('KeyboardInterupt in main function')
+			if self.close is not True:
+				self.close = True
+				# This is for when a KeyboardInterupt happens inside a the "loop.run_forever/loop.run_until_complete" coroutine
+				# This will more likely happen when there is very little work to be done (almost no blocking)
+				loop.run_until_complete(keep_running_till_all_others_complete(loop))
+	
+	
+	
+	
 
 
 class Window:
@@ -50,7 +124,7 @@ class Window:
 	def maxyx(self):
 		return self.text_area.getmaxyx()
 
-class BaseUI:
+class BaseUI1:
 	'''
 		a class that represents the UI and is used as the base for specific UIs
 	'''
@@ -67,16 +141,11 @@ class BaseUI:
 		'''
 			initalizes this UI for use in context managers
 		'''
-		self.main_window = curses.initscr()
-		self.main_window.nodelay(True)
-		self.main_window.keypad(True)
-		curses.noecho()
-		curses.start_color()
-		
-
-		
-
-		
+		#self.main_window = curses.initscr()
+		#self.main_window.nodelay(True)
+		#self.main_window.keypad(True)
+		#curses.noecho()
+		#curses.start_color()
 		self.setup()
 		return self
 		
@@ -99,38 +168,43 @@ class BaseUI:
 		curses.nocbreak()
 		curses.endwin()
 
+
+	def keyboard_interuptable_loop_coroutine(f):
+		async def func_wrapper(*args, **kwargs):
+			self = args[0]
+			print(self)
+			while self.close == False:
+				await asyncio.sleep(0)
+				try:
+					await f(*args, **kwargs)
+				except KeyboardInterrupt as kbi:
+					self.cleanup()
+					self.close = True
+		return func_wrapper
+
 	@property
 	def maxyx(self):
 		return self.main_window.getmaxyx()
-
+	
+	
 	async def screen_updater(self):
 		'''
 			a Job that is used to refresh the screen at the specified frame rate
 		'''
-		while True:
-			try:
-				await asyncio.sleep(self.rate)
-				if self.close:
-					return
-				else:
-					self.pre_update_work()
-					curses.doupdate()
-			except KeyboardInterrupt:
-				self.cleanup()
-				self.close = True
-	
+		await asyncio.sleep(self.rate)
+		self.pre_update_work()
+		#curses.doupdate()
+		
 	def pre_update_work(self):
 		pass
 	
+	@keyboard_interuptable_loop_coroutine
 	async def keyboard_listener(self):
-		while True:
-			key = await self.get_key()
-			if key is None:
-				continue
-			else:
-				self.key_stroke_handler(key)
-			
-	
+		key = await self.get_key()
+		if key is not None:
+			self.key_stroke_handler(key)
+
+
 	async def get_key(self):
 		await asyncio.sleep(0.1)
 		ch = None
@@ -143,9 +217,55 @@ class BaseUI:
 	def key_stroke_handler(self, key):
 		pass 
 
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+class TestUI_1(TaskLoopBase):
+	@keyboard_interuptable_loop_coroutine
+	async def test1(self, name):
+		await asyncio.sleep(1)
+		#time.sleep(1)
+		print(f'running test 1 {name}')
+		
+class TestUI_2(TaskLoopBase):
+	@keyboard_interuptable_loop_coroutine
+	async def test1(self, name):
+		#await asyncio.sleep(1)
+		time.sleep(1)
+		print(f'running test 1 {name}')	
+		
+		
+	
+			
+			
+
+
+
+
 def tests():
-	pass
+	print('starting')
+	with TestUI_1() as ui:
+		ui.add_task(ui.test1('A'))
+		ui.add_task(ui.test1('B'))
+		ui.run_until_complete()
+	
+	with TestUI_2() as ui:
+		ui.add_task(ui.test1('C'))
+		ui.add_task(ui.test1('D'))
+		ui.run_until_complete()
+		
+	print('ending')
 
 
-if __name__=='__name__':
+
+
+
+if __name__=='__main__':
 	tests()
