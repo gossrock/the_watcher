fa43f6242f5eff8bdcd14d867013d2fe3c21d699
gossrock <gossrock@gmail.com>
2017-11-14 12:30:04 +0000

    Added type anotation to command_execution module

--- command_execution.py
+++ command_execution.py
@@ -1,22 +1,29 @@
 import asyncio
-from collections import namedtuple
+import typing
+from typing import NamedTuple, List
 
-CommandResult = namedtuple('CommandResult', ['command', 'out', 'error'])
+class CommandResult(NamedTuple):
+	command:List[str]
+	out:str
+	error:str
 
-async def run_command_str(command_string):
+async def run_command_str(command_string:str) -> CommandResult:
 	split_string = command_string.split(' ')
-	return await run_command(*split_string)
+	return await run_command(split_string)
 	
 
-async def run_command(*args):
-	process = await asyncio.create_subprocess_exec( *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+async def run_command(command:List[str]) -> CommandResult:
+	try:
+		process = await asyncio.create_subprocess_exec( *command, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+	except FileNotFoundError as e:
+		return CommandResult(command, '', str(e))
 	stdout, stderr = await process.communicate()
-	out = stdout.decode().strip()
-	error = stderr.decode().strip()
-	return CommandResult(args, out, error)
+	out:str = stdout.decode().strip()
+	error:str = stderr.decode().strip()
+	return CommandResult(command, out, error)
 	
 	
-def command_to_string(command):
+def command_to_string(command:List[str]) -> str:
 	command_string = ''
 	for part in command:
 		if command_string != '':
@@ -26,11 +33,11 @@ def command_to_string(command):
 	return command_string
 	
 
-def print_command(command):
+def print_command(command:List[str]) -> None:
 	print(command_to_string(command))
 	
 	
-def print_result(result):
+def print_result(result:CommandResult) -> None:
 	print('=============================================')
 	print(f'COMMAND:{command_to_string(result.command)}')
 	if result.out != '':
@@ -40,3 +47,18 @@ def print_result(result):
 		print('STDERROR')
 		print(result.error)
 	print('=============================================')
+
+
+if __name__=='__main__':
+	loop = asyncio.get_event_loop()
+	results = loop.run_until_complete(run_command_str('ls -la'))
+	print_result(results)
+	
+	results = loop.run_until_complete(run_command_str('ping -c 1 firewall'))
+	print_result(results)
+
+	results = loop.run_until_complete(run_command_str('ping -c 1 192.168.0.1'))
+	print_result(results)
+
+	results = loop.run_until_complete(run_command_str('not a command'))
+	print_result(results)
