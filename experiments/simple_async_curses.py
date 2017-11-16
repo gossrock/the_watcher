5e49096cf5ae0997581ffa3d24881892e1390de5
gossrock <gossrock@gmail.com>
2017-11-16 13:18:14 +0000

    revert code to remove panel/screen stuff

--- experiments/simple_async_curses.py
+++ experiments/simple_async_curses.py
@@ -1,6 +1,5 @@
 import asyncio
 import curses
-import curses.panel
 from abc import ABC
 import time
 
@@ -14,8 +13,6 @@ class BaseUI(ABC, TaskLoopBase):
 	def __init__(self, rate:float=1) -> None:
 		super(BaseUI, self).__init__()
 		self.rate:float = rate
-		self.screens:List['BaseScreen'] = []
-		self.active_screen:int = -1
 		
 	def __enter__(self) -> 'BaseUI':
 		self.main_window = curses.initscr()
@@ -64,48 +61,13 @@ class BaseUI(ABC, TaskLoopBase):
 			ch = str(ch)
 		return cast(Union[str,int,None],ch)
 		
-	def key_stroke_handler(self, key:Union[str, int]) -> None:
-		if key == curses.KEY_PPAGE:
-			self.next_screen()
-			
-		elif key == curses.KEY_NPAGE:
-			self.prev_screen()
-		
-	
 	#
-	def add_screen(self, screen:'BaseScreen') -> None:
-		self.screens.append(screen)
-		if self.active_screen == -1:
-			self.active_screen = 0
-		
-	def next_screen(self):
-		screen_to_activate = (self.active_screen + 1) % len(self.screens)
-		self.screens[screen_to_activate].raise_to_top()
-		
-	def prev_screen(self):
-		screen_to_activate = (self.active_screen - 1) % len(self.screens)
-		self.screens[screen_to_activate].raise_to_top()
-		
-	def raise_screen(self, screen):
-		screen.panel.top()
-		self.active_screen = self.screens.index(screen)
 
 
 
 #
 class BaseScreen(ABC):
-	def __init__(self, parent:BaseUI) -> None:
-		self.parent:BaseUI = parent
-		self.parent.add_screen(self)
-		maxy, maxx = self.parent.main_window.getmaxyx()
-		self.window = parent.main_window.derwin(maxy, maxx, 0, 0)
-		self.window.nodelay(True)
-		self.window.keypad(True)
-		
-		self.panel = curses.panel.new_panel(self.window)
-		
-	def raise_to_top(self) -> None:
-		self.parent.raise_screen(self)
+	pass
 		
 	
 class SimplestScreen(BaseScreen):
@@ -124,56 +86,7 @@ class SimplestUI(BaseUI):
 	def key_stroke_handler(self, key:Union[str,int]) -> None:
 		self.main_window.addstr(str(key))
 		self.main_window.noutrefresh()
-
-class ScreenUI(BaseUI):
-	def setup(self) -> None:
-		self.num_screen = SimplestScreen(self)
-		self.add_screen(self.num_screen)
-		
-		self.letter_screen = SimplestScreen(self)
-		self.add_screen(self.letter_screen)
-		
-		self.num_screen.window.addstr('1234567')
-		self.num_screen.window.noutrefresh()
-		curses.panel.update_panels()
-		curses.doupdate()
-		time.sleep(1)
-		
-		self.letter_screen.window.addstr('ABCDEFG')
-		self.letter_screen.window.noutrefresh()
-		curses.panel.update_panels()
-		curses.doupdate()
-		time.sleep(1)
-		
-		while True:
-			print('1')
-			self.letter_screen.panel.hide()
-			self.num_screen.panel.show()
-			curses.panel.update_panels()
-			
-			self.num_screen.window.addstr('B')
-			self.num_screen.window.noutrefresh()
-			
-			curses.doupdate()
-			time.sleep(1)
-			
-			print('2')
-			self.num_screen.panel.hide()
-			self.letter_screen.panel.show()
-			curses.panel.update_panels()
-			
-			self.letter_screen.window.addstr('A')
-			self.letter_screen.window.noutrefresh()
-			curses.panel.update_panels()
-			curses.doupdate()
-			time.sleep(1)
-		
-		
 	
-	def key_stroke_handler(self, key:Union[str,int]) -> None:
-		super(BaseUI, self).key_stroke_handler(key)
-		self.num_screen.window.addstr(str(key))
-		self.num_screen.window.noutrefresh()	
 
 
 
@@ -181,16 +94,11 @@ def test():
 	print('starting tests')
 	
 	print('BaseUI test 1')
-	#with SimplestUI() as ui:
-	#	ui.add_task(ui.keyboard_listener())
-	#	ui.add_task(ui.screen_updater())
-	#	ui.run_until_complete()
-	
-	print('BaseScreen test 1')
-	with ScreenUI() as ui:
+	with SimplestUI() as ui:
 		ui.add_task(ui.keyboard_listener())
 		ui.add_task(ui.screen_updater())
 		ui.run_until_complete()
+
 	
 	print('ending tests')
 	
